bd41a107fe57707432798bb815c8e17abea84874
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-06 12:45:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 //────────────────────────────────────────────────────────────────────────────────────────                                                                                                                                                                                                                                                                */
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Kt69lMVe90WLBkCFcoYIyA']
-global.channelYT = ['https://youtube.com/channel/UCPhU6uA_3gUaEvyrFZFgJqg'] 
-global.owner = ['6283146208804', '62882008097970', '6283125926877']// Put your number here
-global.mods = [ '6283146208804' ,  '62882008097970' ,  '6283146208804' ] // Want some help?
+global.linkGC = ['']
+global.channelYT = [''] 
+global.owner = ['6287832147584','6287832147584']// Put your number here
+global.mods = ['6287832147584' ] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -33,8 +33,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Herman'
-global.author = 'Bot'
+global.packname = '©Pangeran'
+global.author = 'ig: abengrndna_'
 
 global.multiplier = 72366367263636 // The higher, The harder levelup
 
